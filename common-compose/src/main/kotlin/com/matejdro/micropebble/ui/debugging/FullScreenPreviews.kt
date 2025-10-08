e6694eda749215e7d10bf311f789137caa2f3cb5
Matej Drobnič <matej@matejdro.com>
2025-10-08 09:09:02 +0200

    refactor: fix landscape previews

--- common-compose/src/main/kotlin/com/matejdro/micropebble/ui/debugging/FullScreenPreviews.kt
+++ common-compose/src/main/kotlin/com/matejdro/micropebble/ui/debugging/FullScreenPreviews.kt
@@ -13,62 +13,62 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_YES,
-   device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=411dp,height=891dp,dpi=420",
    name = "Night mode portrait"
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_NO,
-   device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=411dp,height=891dp,dpi=420",
    name = "Day mode portrait"
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_YES,
-   device = "spec:id=reference_phone,shape=Normal,width=891,height=411,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=891dp,height=411dp,dpi=420",
    name = "Night mode landscape"
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_NO,
-   device = "spec:id=reference_phone,shape=Normal,width=891,height=411,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=891dp,height=411dp,dpi=420",
    name = "Day mode landscape"
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_YES,
-   device = "spec:id=reference_phone,shape=Normal,width=300,height=600,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=300dp,height=600dp,dpi=420",
    name = "Small night mode portrait"
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_NO,
-   device = "spec:id=reference_phone,shape=Normal,width=300,height=600,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=300dp,height=600dp,dpi=420",
    name = "Small day mode portrait"
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_YES,
-   device = "spec:id=reference_phone,shape=Normal,width=600,height=300,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=600dp,height=300dp,dpi=420",
    name = "Small night mode landscape"
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_NO,
-   device = "spec:id=reference_phone,shape=Normal,width=600,height=300,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=600dp,height=300dp,dpi=420",
    name = "Small day mode landscape"
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_NO,
-   device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=411dp,height=891dp,dpi=420",
    name = "Day mode portrait with large font",
    fontScale = 1.5f
 )
 @Preview(
    showSystemUi = true,
    uiMode = Configuration.UI_MODE_NIGHT_YES,
-   device = "spec:id=reference_phone,shape=Normal,width=891,height=411,unit=dp,dpi=420",
+   device = "spec:id=reference_phone,shape=Normal,width=891dp,height=411dp,dpi=420",
    name = "Night mode landscape with large font",
    fontScale = 1.5f
 )
