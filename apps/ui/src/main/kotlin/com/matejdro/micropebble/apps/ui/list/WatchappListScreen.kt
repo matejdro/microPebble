b860dc66520797a4e71f87696232ed89b2228f21
Matej Drobnič <matej@matejdro.com>
2025-11-05 12:28:12 +0100

    fix(apps): watchapp screen tab resetting after leaving
    this fixes #21

--- apps/ui/src/main/kotlin/com/matejdro/micropebble/apps/ui/list/WatchappListScreen.kt
+++ apps/ui/src/main/kotlin/com/matejdro/micropebble/apps/ui/list/WatchappListScreen.kt
@@ -32,7 +32,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -145,7 +145,7 @@ private fun WatchappListScreenContent(
    openConfiguration: (Uuid) -> Unit,
 ) {
    ErrorAlertDialog(actionStatus, errorText = { it.installUserFriendlyErrorMessage() })
-   var selectedTab by remember { mutableIntStateOf(0) }
+   var selectedTab by rememberSaveable { mutableIntStateOf(0) }
 
    val displayedItems = if (selectedTab == 0) state.watchfaces else state.watchapps
 
