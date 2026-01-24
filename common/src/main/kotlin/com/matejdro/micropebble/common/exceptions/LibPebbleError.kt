eae42a7b03c5b3eac3f5dbd3d5ac627168fae682
Matej Drobnič <matej@matejdro.com>
2026-01-24 08:13:10 +0100

    ci: reproduce sarif failure

--- common/src/main/kotlin/com/matejdro/micropebble/common/exceptions/LibPebbleError.kt
+++ common/src/main/kotlin/com/matejdro/micropebble/common/exceptions/LibPebbleError.kt
@@ -2,5 +2,10 @@ package com.matejdro.micropebble.common.exceptions
 
 import si.inova.kotlinova.core.outcome.CauseException
 
-class LibPebbleError(message: String?, cause: Throwable? = null) :
+class LibPebbleError(
+
+   message: String?,
+
+   cause: Throwable? = null,
+) :
    CauseException(message, cause, isProgrammersFault = message == null)
