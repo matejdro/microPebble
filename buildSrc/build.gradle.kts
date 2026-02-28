396058b94ea4b123acbcf8a8f936d0134821a041
Matej Drobnič <matej@matejdro.com>
2026-02-28 12:08:10 +0100

    build: remove unused tomlj

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
    implementation(libs.moduleGraphAssert)
    implementation(libs.orgJson)
    implementation(libs.ksp)
-   implementation(libs.tomlj)
    implementation(libs.unmock.plugin)
 
    // Workaround to have libs accessible (from https://github.com/gradle/gradle/issues/15383)
