749354d94cc3b25bbc2b695ddd6ab79ea172e842
Matej Drobnič <matej@matejdro.com>
2026-03-18 10:43:03 +0100

    fix(appstore): saved state encoding error
    this is a second stack trace reported in #66

--- appstore/api/src/main/kotlin/com/matejdro/micropebble/appstore/api/serializer/HeaderImagesSerializer.kt
+++ appstore/api/src/main/kotlin/com/matejdro/micropebble/appstore/api/serializer/HeaderImagesSerializer.kt
@@ -1,17 +1,35 @@
 package com.matejdro.micropebble.appstore.api.serializer
 
 import com.matejdro.micropebble.appstore.api.store.application.HeaderImage
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonDecoder
 import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.JsonTransformingSerializer
 
 /**
  * For some reason some parts of the API use an empty string to represent no items instead of an empty list, making this
  * necessary.
  */
-object HeaderImagesSerializer : JsonTransformingSerializer<List<HeaderImage>>(ListSerializer(HeaderImage.serializer())) {
-   override fun transformDeserialize(element: JsonElement) =
-      if (element is JsonPrimitive && element.isString) JsonArray(emptyList()) else element
+object HeaderImagesSerializer : KSerializer<List<HeaderImage>> {
+   private val listSerializer = ListSerializer(HeaderImage.serializer())
+
+   override val descriptor: SerialDescriptor get() = listSerializer.descriptor
+
+   override fun serialize(encoder: Encoder, value: List<HeaderImage>) {
+      listSerializer.serialize(encoder, value)
+   }
+
+   override fun deserialize(decoder: Decoder): List<HeaderImage> {
+      return if (decoder is JsonDecoder) {
+         val element = decoder.decodeJsonElement()
+         val decodedElement = if (element is JsonPrimitive && element.isString) JsonArray(emptyList()) else element
+         json.decodeFromJsonElement(listSerializer, decodedElement)
+      } else {
+         listSerializer.deserialize(decoder)
+      }
+   }
 }
