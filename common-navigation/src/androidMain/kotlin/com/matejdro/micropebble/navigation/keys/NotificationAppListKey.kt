62354144d16a1cae4ea5ade47e552fd55ac276a5
Jakub Jasinski <jakub.jasinski@withintent.com>
2026-05-30 09:21:06 +0200

    KMP-3: introduce shared Tab model and adapt SelectedTabContent

--- common-navigation/src/androidMain/kotlin/com/matejdro/micropebble/navigation/keys/NotificationAppListKey.kt
+++ common-navigation/src/androidMain/kotlin/com/matejdro/micropebble/navigation/keys/NotificationAppListKey.kt
@@ -1,7 +1,11 @@
 package com.matejdro.micropebble.navigation.keys
 
+import com.matejdro.micropebble.navigation.keys.base.Tab
+import com.matejdro.micropebble.navigation.keys.base.TabKey
 import kotlinx.serialization.Serializable
 import si.inova.kotlinova.navigation.screenkeys.ScreenKey
 
 @Serializable
-data object NotificationAppListKey : ScreenKey()
+data object NotificationAppListKey : ScreenKey(), TabKey {
+   override val tab get() = Tab.NOTIFICATIONS
+}
